ef568d11367a173321a9b05bb036dabbeebd5e86
Hamdan Al Wahidan <hamdanalwahidan@gmail.com>
2020-11-02 18:01:43 +0700

    TUGAS

---
+++ TugasLatihan Python LAB2.py
@@ -0,0 +1,11 @@
+a=int(input("Masukan Nilai A ="))
+b=int(input("Masukan Nilai B ="))
+print("Variable A =",a)
+print("Variable B =",b)
+print("Hasil Penggabungan {1}&{0}=%d" .format(a,b) %(a+b))
+
+#Konversi Nilai Variable
+a=int(a)
+b=int(b)
+print("Hasil Penjumlahan {1}+{0}=%d" .format(a,b) %(a+b))
+print("Hasil Pembagian {1}/{0}=%d" .format(a,b) %(a/b))
