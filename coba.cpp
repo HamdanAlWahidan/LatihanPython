ae70802c924f23ca03dab3722a28deb1b8f0f349
Hamdan Al Wahidan <hamdanalwahidan@gmail.com>
2020-11-02 16:08:26 +0700

    coba

---
+++ coba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  int A, B, Hasil;
+  std::cout << "Masukan Nilai A =" << '\n';
+  std::cin >> A;
+  std::cout << "Masukan Nilai B =" << '\n';
+  std::cin >> B;
+  Hasil=A+B;
+  std::cout << "Hasil =" << Hasil << '\n';
+  return 0;
+}
